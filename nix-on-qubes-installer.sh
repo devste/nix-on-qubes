3317c198c8f0c49a3b8cbd28352dbcf4aa2d8ffc
Stephen Tallowitz <devste@mailbox.org>
2023-02-11 01:15:04 +0100

    Switch to a more stable channel after installation

--- nix-on-qubes-installer.sh
+++ nix-on-qubes-installer.sh
@@ -5,6 +5,9 @@ nix_dir="/nix"
 nix_version="2.13.2"
 nix_install_src="https://releases.nixos.org/nix/nix-${nix_version}/install"
 
+nix_os_version="22.11"
+nix_os_channel="https://channels.nixos.org/nixos-${nix_os_version}"
+
 cd "${HOME}" || exit
 
 # Create the nix-specific bind-dir configuration for qubes
@@ -22,4 +25,10 @@ echo "Downloading nix installer from ${nix_install_src}"
 curl --proto '=https' --tlsv1.2 "${nix_install_src}" -o "nix-installer-${nix_version}.sh"
 chmod u+x nix-installer-${nix_version}.sh
 ./nix-installer-${nix_version}.sh \
-	--no-daemon
+	--no-daemon \
+	--no-channel-add
+
+# Add a stable channel as nixpkgs
+source "${HOME}/.profile"
+nix-channel --add "${nix_os_channel}" nixpkgs
+nix-channel --update
